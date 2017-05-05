c126dd29d7994cbcfc7dc93ab73f3803d0f485af
Adam Barrett <adamlbarrett@hotmail.com>
2017-05-05 15:53:20 -0600

    implement noop for action

--- src/can-x.js
+++ src/can-x.js
@@ -2,7 +2,9 @@ import Provider from './provider';
 import inject from './inject';
 import observer from './observer';
 
-export function action() {}
+export function action(target) {
+	return target;
+}
 
 export {inject, observer, Provider};
 
